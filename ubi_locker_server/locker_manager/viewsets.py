7795e660a881da923884b21f886ca1ed3583897d
Francielly Cardoso <ciellycardoso@gmail.com>
2016-05-05 18:47:04 -0300

    add endpoint to change RFID's Person to a constant value

--- ubi_locker_server/locker_manager/viewsets.py
+++ ubi_locker_server/locker_manager/viewsets.py
@@ -5,9 +5,10 @@ from .serializers import PersonSerializer
 from rest_framework.decorators import detail_route
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import exceptions
+from rest_framework import status
+from rest_framework.response import Response
 
 class PersonViewSet(viewsets.ModelViewSet):
     """
@@ -18,8 +19,11 @@ class PersonViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     @detail_route(methods=['post'])
-    def set_RFID(self, request, rfid):
+    def set_RFID(self, request, *args, **kwargs):
 		person = self.get_object()
-		person.RFID = rfid
+		#rfid = kwargs['rfid']
+		#person.RFID = rfid
+		setattr(person, 'RFID', '999')
+		person.save()
 		content = {'message':'RFID upddated.'}
 		return Response(content, status=status.HTTP_200_OK)
