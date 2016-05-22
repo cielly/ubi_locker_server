e5aaf498205b5ca3f6ba1784686c486fce3c76a9
Francielly Cardoso <ciellycardoso@gmail.com>
2016-05-22 13:18:10 -0300

    change heroku

---
+++ ubi_locker_server/ubi_locker_server/local_settings.py
@@ -0,0 +1,15 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    'default': {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ubi_locker',
+        'USER': 'ubi_locker_user',
+        'PASSWORD': 'pass123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
--- ubi_locker_server/ubi_locker_server/settings.py
+++ ubi_locker_server/ubi_locker_server/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 fillPath = lambda x: os.path.join(os.path.dirname(__file__), x)
@@ -159,3 +160,17 @@ LOGIN_EXEMPT_URLS = (
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.auth',
 )
+
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
